8728fbb40826c0025c11e02a0a4d07322add9f6b
Sulaiman Tahir Idris <sulaymanti01@gmail.com>
2023-06-27 22:05:06 +0100

    fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((data) => {
-      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
+
     .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
